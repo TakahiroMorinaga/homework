70c72cf0c3bc008eb9826d7db7242dbde32839db
TakahiroMorinaga <97648514+TakahiroMorinaga@users.noreply.github.com>
2022-01-18 23:54:07 +0900

    Add files via upload

--- kadai2.py
+++ kadai2.py
@@ -19,6 +19,8 @@
 #UIの設定
 
 import tkinter as tk
+import tkinter.messagebox as msg
+import pandas as pd
 import csv
 
 
@@ -27,30 +29,55 @@ import csv
 
 
 
-def signup_button_pushed_on_canvas2(widget):
-    dataset = []
+def signup_button_pushed_on_canvas2():
+
+    name=str_name.get()
+    UserID=str_UserID.get()
+    email=str_email.get()
+    age=int_age.get()
+    height=int_height.get()
+
+    if name=='' or UserID=='' or email=='' or age=='' or height=='':
+        msg.showinfo("お知らせ","データが未入力です。")
+    else:
+        df=pd.DataFrame({"name":[name],"UserID":[UserID],"email":[email],"age":[age],"height":[height]})
+        df.to_csv("kadai2data.csv",mode="a",header=False,index=False)
+        msg.showinfo("お知らせ","登録が完了しました。")
+
+#ユーザ入力値の保持
+#str_name=tk.StringVar()
+#str_UserID=tk.StringVar()
+#str_email=tk.StringVar()
+#int_age=tk.IntringVar()
+#int_height=Int.StringVar()
+
+
+
+#   dataset =[]
     
-    canvas2txt2=tk.Entry(root,width=21) 
-    dataset.append(canvas2txt2.get())
+#    canvas2txt2=tk.Entry(root,width=21) 
+#    dataset.append(canvas2txt2.get())
+    
 
-    canvas2txt3=tk.Entry(root,width=21)
-    dataset.append(canvas2txt3.get())
+#    canvas2txt3=tk.Entry(root,width=21)
+#    dataset.append(canvas2txt3.get())
 
-    canvas2txt4=tk.Entry(root,width=21)
-    dataset.append(canvas2txt4.get())
+#    canvas2txt4=tk.Entry(root,width=21)
+#    dataset.append(canvas2txt4.get())
 
-    canvas2txt5=tk.Entry(root,width=21)
-    dataset.append(canvas2txt5.get())
+#    canvas2txt5=tk.Entry(root,width=21)
+#    dataset.append(canvas2txt5.get())
 
-    canvas2txt6=tk.Entry(root,width=21)
-    dataset.append(canvas2txt6.get())
+#    canvas2txt6=tk.Entry(root,width=21)
+#    dataset.append(canvas2txt6.get())
 
-    with open("kadai2data.csv","a",newline="") as csvFile:
-        writer=csv.writer(csvFile)
-        writer.writerow(dataset)
+#    with open("kadai2data.csv","a",newline="") as csvFile:
+#        writer=csv.writer(csvFile)
+#       writer.writerow(dataset)
+#        print(dataset)
 
 
-    return
+ #  return
 
 
 
@@ -72,30 +99,31 @@ def transition_button_signup(widget):
 
     canvas2lbl2=tk.Label(root,text="Enter your Name",fg="black",font=("MSゴシック",12,"bold"))
     canvas2lbl2.place(x=100,y=100)
-    canvas2txt2=tk.Entry(root,width=21)
+    canvas2txt2=tk.Entry(root,width=21,textvariable=str_name)
     canvas2txt2.place(x=220,y=100)
+    
 
     canvas2lbl3=tk.Label(root,text="Enter User ID",fg="black",font=("MSゴシック",12,"bold"))
     canvas2lbl3.place(x=100,y=140)
-    canvas2txt3=tk.Entry(root,width=21)
+    canvas2txt3=tk.Entry(root,width=21,textvariable=str_UserID)
     canvas2txt3.place(x=220,y=140)
 
     canvas2lbl4=tk.Label(root,text="Enter your email",fg="black",font=("MSゴシック",12,"bold"))
     canvas2lbl4.place(x=100,y=180)
-    canvas2txt4=tk.Entry(root,width=21)
+    canvas2txt4=tk.Entry(root,width=21,textvariable=str_email)
     canvas2txt4.place(x=220,y=180)
 
     canvas2lbl5=tk.Label(root,text="Enter your age",fg="black",font=("MSゴシック",12,"bold"))
     canvas2lbl5.place(x=100,y=220)
-    canvas2txt5=tk.Entry(root,width=21)
+    canvas2txt5=tk.Entry(root,width=21,textvariable=int_age)
     canvas2txt5.place(x=220,y=220)
 
     canvas2lbl6=tk.Label(root,text="Enter your height",fg="black",font=("MSゴシック",12,"bold"))
     canvas2lbl6.place(x=100,y=260)
-    canvas2txt6=tk.Entry(root,width=21)
+    canvas2txt6=tk.Entry(root,width=21,textvariable=int_height)
     canvas2txt6.place(x=220,y=260)
 
-    btn2btn1=tk.Button(canvas2,text="SIGN UP",width=11,height=3,command=lambda:signup_button_pushed_on_canvas2(widget),font=("MSゴシック",15,"bold"),fg="green")
+    btn2btn1=tk.Button(canvas2,text="SIGN UP",width=11,height=3,command=signup_button_pushed_on_canvas2,font=("MSゴシック",15,"bold"),fg="green")
     btn2btn1.place(x=170,y=310)
 
 #def signup_button_pushed_on_canvas2(widget):
@@ -205,7 +233,11 @@ btn2=tk.Button(canvas1,text="SIGN UP",width=11, height=3,command=lambda:transiti
 btn2.pack()
 btn2.place(x=280,y=310)
 
-
+str_name=tk.StringVar()
+str_UserID=tk.StringVar()
+str_email=tk.StringVar()
+int_age=tk.StringVar()
+int_height=tk.StringVar()
 
 
 #root.mainloop()
